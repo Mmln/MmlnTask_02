356dbd01dcf82516ea06cad0c23bb7485be6358e
Dmitrii Mamylin <dmamylin@gmail.com>
2024-03-20 18:38:51 +0400

    Initial commit

---
+++ src/main/java/com/orgexample/App.java
@@ -0,0 +1,15 @@
+package com.orgexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "MmlnTask_02 started..." );
+
+        System.out.println( "MmlnTask_02 finshed..." );
+    }
+}
